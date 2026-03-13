19db7d16fae771fb8b8aa043ebcce5bc9133077d
Isabella <isabelladuarteassis@gmail.com>
2026-03-13 15:03:50 -0300

    feat(produtos): implementa GET /produtos/:id com tratamento de 404

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -5,7 +5,17 @@ function listar(req, res) {
     res.status(200).json(produtos)
 }
 
-function buscarPorId(req, res) {}
+function buscarPorId(req, res) {
+    const id = parseInt(req.params.id)
+
+    const produto = produtos.find(p => p.id === id)
+
+    if (!produto) {
+        return res.status(404).json({ erro: "Produto não encontrado" })
+    }
+
+    res.status(200).json(produto)
+}
 
 function criar(req, res) {}
 
