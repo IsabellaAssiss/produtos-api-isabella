5a10447f367974ce874c700dc962f5c4f7e6a023
Isabella <isabelladuarteassis@gmail.com>
2026-03-13 14:53:27 -0300

    feat(produtos): cria router e registra rotas no app.js

--- src/app.js
+++ src/app.js
@@ -7,3 +7,7 @@ app.use(express.json())
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000")
 })
+
+const produtosRoutes = require('./routes/produtos')
+
+app.use('/api/v1/produtos', produtosRoutes)
